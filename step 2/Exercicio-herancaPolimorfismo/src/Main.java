8506d883adfc8539619ea22a1da1eab39b67e996
Kayo Weiber <caioveiber598@gmail.com>
2025-06-03 19:30:58 -0300

    feat(main) - test completion

--- step 2/Exercicio-herancaPolimorfismo/src/Main.java
+++ step 2/Exercicio-herancaPolimorfismo/src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Conta testeConta = new Conta(1,1000);
@@ -7,6 +6,12 @@ public class Main {
         testeConta.ExibirSaldo();
         testeConta.saque(200);
         testeConta.ExibirSaldo();
+
+        ContaEspecial testeContaEspecial = new ContaEspecial(1,1000,100);
+        testeContaEspecial.deposito(100);
+        testeContaEspecial.ExibirSaldo();
+        testeContaEspecial.saque(900);
+        testeContaEspecial.ExibirSaldo();
 
 
 }}
